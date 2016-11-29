1b67147bf517bcdf2464965d2d559e23a2886c2b
Khemri Tolya <khemritolya@gmail.com>
2016-11-29 12:02:35 -0500

    Things

---
+++ app/src/main/java/me/composter/rumpus/Launcher.java
@@ -0,0 +1,17 @@
+package me.composter.rumpus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.nope.example.rumpus.R;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        
+    }
+}
