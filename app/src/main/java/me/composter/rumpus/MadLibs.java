03d7d7076f0e92840f5c15f22b948e938071cdd8
Khemri Tolya <khemritolya@gmail.com>
2016-12-02 12:21:33 -0500

    Better Data structure

--- app/src/main/java/me/composter/rumpus/MadLibs.java
+++ app/src/main/java/me/composter/rumpus/MadLibs.java
@@ -2,8 +2,62 @@ package me.composter.rumpus;
 
 public class MadLibs {
 
-    public static final String[] EXAMPLE_1 = {"Hello "," !"};
-    public static final String[] EXAMPLE_2 = {"Then Mr. ", " went to the ", " and decided to ", " on the ","."};
+    public static final String[][] stories = {
+            { "EXMAPLE.COM" , "There was once a young ", "NOUN", " of Wright, who travelled much faster than ", "NOUN",
+                    ", and, departing one day, in a relative way, had arrived on the previous ", "NOUN", "!" }
+    };
 
-    public static final String[][] all = {EXAMPLE_1, EXAMPLE_2};
+    private static final String[] toReplace = {
+            "NOUN", "VERB"
+    };
+
+    private int storyId;
+
+    public MadLibs(int storyID) {
+        this.storyId = storyID;
+    }
+
+    public String[] requestInputs() {
+        String[] res = new String[countOthers(stories[storyId])];
+
+        int j = 0;
+        for (int i = 0; i < stories[storyId].length; i++) {
+            res[j] = stories[storyId][i];
+        }
+
+        return res;
+    }
+
+    public String assemble() {
+        String res = "";
+
+        for (int i = 0; i < stories[storyId].length; i++)
+            res += stories[storyId][i];
+
+        return res;
+    }
+
+    private boolean isOther(String in) {
+        boolean res = false;
+
+        for (int i = 0; i < toReplace.length; i++) {
+            if (in.equals(toReplace[i])){
+                res = false;
+                break;
+            }
+        }
+
+        return res;
+    }
+
+    private int countOthers(String[] story) {
+        int i = 0;
+
+        for (int j = 0; j < story.length; j++) {
+            if (isOther(story[j]))
+                i++;
+        }
+
+        return i;
+    }
 }
