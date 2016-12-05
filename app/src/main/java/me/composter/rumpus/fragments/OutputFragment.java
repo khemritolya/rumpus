2f8e90949b40668aeb513bf2654073380ed96ce3
Khemri Tolya <khemritolya@gmail.com>
2016-12-05 11:24:41 -0500

    More things

--- app/src/main/java/me/composter/rumpus/fragments/OutputFragment.java
+++ app/src/main/java/me/composter/rumpus/fragments/OutputFragment.java
@@ -1,30 +1,20 @@
 package me.composter.rumpus.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import me.composter.rumpus.R;
-
+import me.composter.rumpus.MadLibs;
 
 public class OutputFragment extends Fragment {
+
     public OutputFragment() {
-        // Required empty public constructor
-    }
-
-    public static OutputFragment newInstance() {
-        OutputFragment fragment = new OutputFragment();
-
-        return fragment;
+        super();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 }
