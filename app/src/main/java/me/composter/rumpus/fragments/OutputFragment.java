fc28c190961f03d6689df8fd80a7a5a8946db695
Khemri Tolya <khemritolya@gmail.com>
2016-12-08 09:21:37 -0500

    Output should be fine, or something

--- app/src/main/java/me/composter/rumpus/fragments/OutputFragment.java
+++ app/src/main/java/me/composter/rumpus/fragments/OutputFragment.java
@@ -1,12 +1,13 @@
 package me.composter.rumpus.fragments;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
 import android.widget.TextView;
 
 import org.nope.example.rumpus.R;
 
-import me.composter.rumpus.MadLibs;
+import me.composter.rumpus.Launcher;
 
 public class OutputFragment extends Fragment {
 
@@ -23,5 +24,6 @@ public class OutputFragment extends Fragment {
         TextView t = (TextView) getView().findViewById(R.id.viewThing);
         t.setText(story);
 
+        ((Launcher) this.getActivity()).tts.speak(story, TextToSpeech.QUEUE_FLUSH, null, this.hashCode() + ":" + story);
     }
 }
