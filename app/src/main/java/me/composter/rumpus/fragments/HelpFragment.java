7a9a616ddbc2d0f874c3d45ae0c625921cfa5297
Khemri Tolya <khemritolya@gmail.com>
2016-12-20 12:12:33 -0500

    Ok Mk 2

--- app/src/main/java/me/composter/rumpus/fragments/HelpFragment.java
+++ app/src/main/java/me/composter/rumpus/fragments/HelpFragment.java
@@ -2,24 +2,47 @@ package me.composter.rumpus.fragments;
 
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import org.nope.example.rumpus.R;
 
-import me.composter.rumpus.R;
+import me.composter.rumpus.Launcher;
 
 public class HelpFragment extends Fragment {
 
 
     public HelpFragment() {}
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View v = inflater.inflate(R.layout.fragment_help, container, false);
+
+        Button b = (Button)v.findViewById(R.id.nounButton);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = "A noun is an idea or an objct: Like chair, or water, or cow, or ipad";
+                ((Launcher) getActivity()).tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, this.hashCode() + text);
+            }
+        });
+
+        b = (Button)v.findViewById(R.id.verbButton);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = "A verb is an action word: Like run, or fly, or give, or say";
+                ((Launcher) getActivity()).tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, this.hashCode() + text);
+            }
+        });
+
+        return v;
     }
 
 }
